946c1d0506f56bf9d5a9c6497456ddd35ab314f9
eunbi <geunbi38@gmail.com>
2026-03-26 01:09:18 +0900

    SGTaggedActor C++, BP 클래스 생성. B_PickRandomCharacter에서 PawnComp_CharacterParts::AddCharacterPart() 호출해서 B_Manny 장착 구현.

---
+++ SG/Source/SG/AbilitySystem/SGTaggedActor.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SG/AbilitySystem/SGTaggedActor.h"
+
+ASGTaggedActor::ASGTaggedActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASGTaggedActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASGTaggedActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ SG/Source/SG/AbilitySystem/SGTaggedActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SGTaggedActor.generated.h"
+
+UCLASS()
+class SG_API ASGTaggedActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASGTaggedActor();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+};
