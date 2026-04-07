daa91753210c340f4ed72e9cc182aca6e7982077
eunbi <geunbi38@gmail.com>
2026-04-08 02:39:34 +0900

    SGGA_FromEquipment의 C++ 클래스, SGGA_RangedWeapon의 C++, BP 클래스 생성.

---
+++ SG/Source/SG/Equipment/SGGA_FromEquipment.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SG/AbilitySystem/Abilities/SGGameplayAbility.h"
+#include "SGGA_FromEquipment.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SG_API USGGA_FromEquipment : public USGGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
---
+++ SG/Source/SG/Weapons/SGGA_RangedWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SG/Equipment/SGGA_FromEquipment.h"
+#include "SGGA_RangedWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SG_API USGGA_RangedWeapon : public USGGA_FromEquipment
+{
+	GENERATED_BODY()
+	
+};
