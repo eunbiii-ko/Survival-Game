36b9bc087009aff7e600eefd4cb8751cb5b279d6
eunbi <geunbi38@gmail.com>
2026-04-15 03:28:23 +0900

    리타겟팅으로 애니메이션 임포트 후, AnimLayerInterface를 이용하여 무기 미장착 상태일 때 애니메이션 구현.

--- SG/Source/SG/Animation/SGAnimInstance.h
+++ SG/Source/SG/Animation/SGAnimInstance.h
@@ -24,10 +24,46 @@ public:
 	//~ End of member methods
 	
 	/** 해당 속성값은 Lyra의 AnimBP에서 사용되는 값이다. */
-	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
 	float GroundDistance = -1.f;
 
 	/** GameplayTag와 AnimInstance의 속성값을 매핑해준다. */
 	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
 	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float AnimPitch = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float GroundSpeed = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float Direction = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float LeftClavicleAnimMultiplier = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float RightClavicleAnimMultiplier = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float LeftClavicleAnimAdditive = 0.f;
+	
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float RightClavicleAnimAdditive = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	float Rotation = 0.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	bool bShouldMove = false;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	bool bIsCrouching = false;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
+	bool bIsFalling = false;
 };
