9b6a7711bd58e60e29f17a4a06db11ab4c7c8725
eunbi <geunbi38@gmail.com>
2026-04-14 15:25:49 +0900

    StartCore의 기본 메시 수정: Mannequin -> Women.

--- SG/Source/SG/Animation/SGAnimInstance.cpp
+++ SG/Source/SG/Animation/SGAnimInstance.cpp
@@ -2,4 +2,25 @@
 
 
 #include "SG/Animation/SGAnimInstance.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
 
+void USGAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (AActor* OwningActor = GetOwningActor())
+	{
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		{
+			InitializeWithAbilitySystem(ASC);
+		}
+	}
+}
+
+void USGAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	// ASC 내부 관리하는 GameplayTag와 Animation의 멤버 Property와 Delegate를 연결하여
+	// 값 변화에 대한 반영을 진행한다. 
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
--- SG/Source/SG/Animation/SGAnimInstance.h
+++ SG/Source/SG/Animation/SGAnimInstance.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
 #include "Animation/AnimInstance.h"
 #include "SGAnimInstance.generated.h"
 
@@ -14,7 +15,19 @@ class SG_API USGAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 public:
+	//~ AnimInstance's interface
+	virtual void NativeInitializeAnimation() override;
+	//~ End of AnimInstance's interface
+
+	//~ member methods
+	void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+	//~ End of member methods
+	
 	/** 해당 속성값은 Lyra의 AnimBP에서 사용되는 값이다. */
 	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
 	float GroundDistance = -1.f;
+
+	/** GameplayTag와 AnimInstance의 속성값을 매핑해준다. */
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
 };
