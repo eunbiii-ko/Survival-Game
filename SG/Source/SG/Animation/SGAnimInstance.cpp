594ed31ea2aa46aa46289c9a88a9fb748149589a
eunbi <geunbi38@gmail.com>
2026-04-18 22:53:19 +0900

    기존 AnimLayerBase 클래스들 삭제하고, 새로 생성해서 에셋에 적용.

--- SG/Source/SG/Animation/SGAnimInstance.cpp
+++ SG/Source/SG/Animation/SGAnimInstance.cpp
@@ -4,18 +4,48 @@
 #include "SG/Animation/SGAnimInstance.h"
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemGlobals.h"
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "SG/SGLogChannels.h"
 
 void USGAnimInstance::NativeInitializeAnimation()
 {
 	Super::NativeInitializeAnimation();
 
-	if (AActor* OwningActor = GetOwningActor())
+	if (OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner()))
 	{
-		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwningActor))
+		if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwnerCharacter))
 		{
 			InitializeWithAbilitySystem(ASC);
 		}
+
+		OwnerMovementComp = OwnerCharacter->GetCharacterMovement();
 	}
+}
+
+void USGAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (OwnerCharacter)
+	{
+		Velocity = OwnerCharacter->GetVelocity();
+		GroundSpeed = Velocity.Size2D();
+		Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, OwnerCharacter->GetActorRotation());
+		Rotation = UKismetMathLibrary::NormalizedDeltaRotator(UKismetMathLibrary::MakeRotFromX(Velocity), OwnerCharacter->GetActorRotation()).Yaw;
+		bShouldMove = (GroundSpeed > 0.f);
+		UE_LOG(LogSG, Display, TEXT("Speed: %f in %d"), GroundSpeed, OwnerCharacter->HasAuthority());
+	}
+	
+	if (OwnerMovementComp)
+	{
+		bIsFalling = OwnerMovementComp->IsFalling();
+		bIsCrouching = OwnerMovementComp->IsCrouching();
+		bShouldMove = true;
+	}
+
 }
 
 void USGAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
--- SG/Source/SG/Animation/SGAnimInstance.h
+++ SG/Source/SG/Animation/SGAnimInstance.h
@@ -7,6 +7,8 @@
 #include "Animation/AnimInstance.h"
 #include "SGAnimInstance.generated.h"
 
+class UCharacterMovementComponent;
+
 /**
  * 
  */
@@ -17,40 +19,29 @@ class SG_API USGAnimInstance : public UAnimInstance
 public:
 	//~ AnimInstance's interface
 	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 	//~ End of AnimInstance's interface
 
 	//~ member methods
 	void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
 	//~ End of member methods
-	
-	/** 해당 속성값은 Lyra의 AnimBP에서 사용되는 값이다. */
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float GroundDistance = -1.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character")
+	TObjectPtr<ACharacter> OwnerCharacter;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Character")
+	TObjectPtr<UCharacterMovementComponent> OwnerMovementComp;
 
 	/** GameplayTag와 AnimInstance의 속성값을 매핑해준다. */
 	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
 	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
 
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float AnimPitch = 0.f;
-
+	
 	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
 	float GroundSpeed = 0.f;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
 	float Direction = 0.f;
-
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float LeftClavicleAnimMultiplier = 0.f;
-
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float RightClavicleAnimMultiplier = 0.f;
-
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float LeftClavicleAnimAdditive = 0.f;
-	
-	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
-	float RightClavicleAnimAdditive = 0.f;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Character State Data")
 	float Rotation = 0.f;
